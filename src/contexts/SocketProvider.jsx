1ef11679664fcda4dd9e629d02f073e5d9470e6d
Arjen Klaverstijn <arjenklaverstijn@gmail.com>
2023-04-05 00:53:02 +0200

    added some basic form of auth

--- src/contexts/SocketProvider.jsx
+++ src/contexts/SocketProvider.jsx
@@ -1,17 +1,26 @@
 import { io } from "socket.io-client";
-import React from "react";
+import React, { useRef } from "react";
 import RemoteConfig from "../config/Config";
+import { getUser } from "../hooks/useLocalStorage";
+import { useEffect } from "react";
+
+const storedUser = getUser("user", null);
 
 export const socket = io.connect(RemoteConfig.SERVER_URL, {
   extraHeaders: {
     clientType: "remote",
   },
+  auth: { token: storedUser?.token },
 });
 
 export const SocketContext = React.createContext(null);
 
 export function SocketProvider({ children }) {
+  const socketRef = useRef(socket);
+
   return (
-    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+    <SocketContext.Provider value={socketRef.current}>
+      {children}
+    </SocketContext.Provider>
   );
 }
